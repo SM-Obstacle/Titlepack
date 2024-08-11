c1965623e6e147e6c060d43a619db085af016102
Ahmad Baalbaky <ahmadjfbky@gmail.com>
2024-08-11 23:15:28 +0200

    fix: disable records in editor

--- Scripts/Libs/smokegun/Settings.py
+++ Scripts/Libs/smokegun/Settings.py
@@ -3,11 +3,13 @@ model = {
 		'TimeLimit', 'ScoreMode', 'RoundLimit', 'MinPlayers',
 		'PointLimit', 'PointsMax', 'PointsMin', 'PointsDeduced',
 		'RoundCooldown', 'WarmupRounds', 'WarmupRoundsTime', 'MinTimeLeft',
-		'MaxTimeLeft', 'CheckpointMarkersCount', 'EventEdition', 'TimeBeforeSwitchEvent'],
+		'MaxTimeLeft', 'CheckpointMarkersCount', 'EventEdition', 'TimeBeforeSwitchEvent',
+	],
 	'Booleans': [
 		'FastRespawn', 'AllowJump', 'EnableRecords', 'LegacyRespawn',
 		'FullAmmoSpawn', 'PointDistribution', 'ForceInvisibleSkin', 'EnablePlayerHighlight',
-		'ForcePvPWeapons', 'ForcePvPCollisions', 'LockEventsUI'],
+		'ForcePvPWeapons', 'ForcePvPCollisions', 'LockEventsUI', 'ForceDisableRecords',
+	],
 	'Reals': ['NextMapVotingRatio', 'SwitchEventVotingRatio'],
 	'Texts': ['BestNewRecordMsg', 'WorseNewRecordMsg', 'ForceRNGSeed', 'EventHandle']
 }
